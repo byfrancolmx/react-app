210904bb511a184fc9459b14a012e06bc914339d
byfrancolmx <byfrancolmx@gmail.com>
2024-08-08 17:03:07 -0300

    first commit

---
+++ src/components/HeadlineCards.jsx
@@ -0,0 +1,58 @@
+import React from "react";
+
+const HeadlineCards = () => {
+  return (
+    <div className="max-w-[1640px] mx-auto p-4 py-12 grid md:grid-cols-3 gap-6">
+      {/* card */}
+      <div className="rounded-xl relative">
+        {/* overlay */}
+        <div className="absolute w-full h-full bg-black/50 rounded-xl text-white">
+          <p className="font-bold text-2xl px-2 pt-4">Sun's Out, BOGO's Out</p>
+          <p className="px-2">Through 8/26</p>
+          <button className="border-white bg-white text-black rounded-md mx-2 p-1 absolute bottom-4">
+            Order Now
+          </button>
+        </div>
+        <img
+          className="max-h-[160px] md:max-h-[200px] w-full object-cover rounded-xl"
+          src="https://images.pexels.com/photos/461198/pexels-photo-461198.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+          alt="/"
+        />
+      </div>
+      {/* card */}
+      <div className="rounded-xl relative">
+        {/* overlay */}
+        <div className="absolute w-full h-full bg-black/50 rounded-xl text-white">
+          <p className="font-bold text-2xl px-2 pt-4">New restaurants</p>
+          <p className="px-2">Added daily</p>
+          <button className="border-white bg-white text-black rounded-md mx-2 p-1 absolute bottom-4">
+            Order Now
+          </button>
+        </div>
+        <img
+          className="max-h-[160px] md:max-h-[200px] w-full object-cover rounded-xl"
+          src="https://images.pexels.com/photos/842571/pexels-photo-842571.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+          alt="/"
+        />
+      </div>
+      {/* card */}
+      <div className="rounded-xl relative">
+        {/* overlay */}
+        <div className="absolute w-full h-full bg-black/50 rounded-xl text-white">
+          <p className="font-bold text-2xl px-2 pt-4">we deliver desserts</p>
+          <p className="px-2">Tasty treats</p>
+          <button className="border-white bg-white text-black rounded-md mx-2 p-1 absolute bottom-4">
+            Order Now
+          </button>
+        </div>
+        <img
+          className="max-h-[160px] md:max-h-[200px] w-full object-cover rounded-xl"
+          src="https://images.pexels.com/photos/323682/pexels-photo-323682.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+          alt="/"
+        />
+      </div>
+    </div>
+  );
+};
+
+export default HeadlineCards;
---
+++ src/data/Data.js
@@ -0,0 +1,177 @@
+export const data = [
+  {
+    id: 1,
+    name: "Double Cheeseburger",
+    category: "burguer",
+    image:
+      "https://images.pexels.com/photos/1251198/pexels-photo-1251198.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+    price: "$$",
+  },
+  {
+    id: 2,
+    name: "Bacon Cheeseburger",
+    category: "burger",
+    image:
+      "https://images.pexels.com/photos/2983099/pexels-photo-2983099.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+    price: "$$",
+  },
+  {
+    id: 3,
+    name: "Mushroom Burger",
+    category: "burger",
+    image:
+      "https://images.pexels.com/photos/12420469/pexels-photo-12420469.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+    price: "$$",
+  },
+  {
+    id: 4,
+    name: "Loaded Burger",
+    category: "burger",
+    image:
+      "https://images.pexels.com/photos/17518459/pexels-photo-17518459/free-photo-of-hamburguesa-shambatore.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+    price: "$$",
+  },
+  {
+    id: 5,
+    name: "Feta & Spinnach",
+    category: "pizza",
+    image:
+      "https://images.pexels.com/photos/25315530/pexels-photo-25315530/free-photo-of-pizza-comida-caja-queso.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+    price: "$$",
+  },
+  {
+    id: 6,
+    name: "Supreme Pizza",
+    category: "pizza",
+    image:
+      "https://images.pexels.com/photos/845811/pexels-photo-845811.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+    price: "$$",
+  },
+  {
+    id: 7,
+    name: "Meat Lovers",
+    category: "pizza",
+    image:
+      "https://images.pexels.com/photos/6151210/pexels-photo-6151210.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+    price: "$$",
+  },
+  {
+    id: 8,
+    name: "Cheese Pizza",
+    category: "pizza",
+    image:
+      "https://images.pexels.com/photos/3644/pizza-restaurant-dinner-lunch.jpg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+    price: "$$",
+  },
+  {
+    id: 9,
+    name: "kale Salad",
+    category: "salads",
+    image:
+      "https://images.pexels.com/photos/6094524/pexels-photo-6094524.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+    price: "$$",
+  },
+  {
+    id: 10,
+    name: "Ceasar Salad",
+    category: "salads",
+    image:
+      "https://images.pexels.com/photos/7601405/pexels-photo-7601405.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+    price: "$$",
+  },
+  {
+    id: 11,
+    name: "Loaded Salad",
+    category: "salads",
+    image:
+      "https://images.pexels.com/photos/24706519/pexels-photo-24706519/free-photo-of-comida-plato-ensalada-italiano.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+    price: "$$",
+  },
+  {
+    id: 12,
+    name: "Fruit Salad",
+    category: "salads",
+    image:
+      "https://images.pexels.com/photos/1546896/pexels-photo-1546896.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+    price: "$$",
+  },
+  {
+    id: 13,
+    name: "Wings",
+    category: "chicken",
+    image:
+      "https://images.pexels.com/photos/24182616/pexels-photo-24182616/free-photo-of-comida-carne-fresco-bandeja.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+    price: "$$",
+  },
+  {
+    id: 14,
+    name: "Baked Chicken",
+    category: "chicken",
+    image:
+      "https://images.pexels.com/photos/17942486/pexels-photo-17942486/free-photo-of-ensaladas-saludables.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+    price: "$$",
+  },
+  {
+    id: 15,
+    name: "Chicken Tenders",
+    category: "chicken",
+    image:
+      "https://images.pexels.com/photos/17942482/pexels-photo-17942482/free-photo-of-vegetales-verduras-almuerzo-comida.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+    price: "$$",
+  },
+  {
+    id: 16,
+    name: "Chicken Kabob",
+    category: "chicken",
+    image:
+      "https://images.pexels.com/photos/24182336/pexels-photo-24182336/free-photo-of-comida-mexicano-carne-bol.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+    price: "$$",
+  },
+];
+
+export const categories = [
+  {
+    id: 1,
+    name: "Fast food",
+    category:
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT47Opba0p_WU8F2S_UK_PpExksgeJyIEilKA&s",
+  },
+  {
+    id: 2,
+    name: "Pizza",
+    image:
+      "https://preview.free3d.com/img/2015/01/1734195376191178108/7yk4upoq.jpg",
+  },
+  {
+    id: 3,
+    name: "Wings",
+    image:
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQEDm-vWKL8sSXAp5yVSgh3AkoY0yDjnFja0A&s",
+  },
+  {
+    id: 4,
+    name: "Salad",
+    image:
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRnwPJhTWtenp60as4ysS2_i4X1GLkW1rfxag&s",
+  },
+  {
+    id: 5,
+    name: "Latest deals",
+    image: "",
+  },
+  {
+    id: 6,
+    name: "ast food",
+    image: "",
+  },
+  {
+    id: 7,
+    name: "Best overall",
+    image: "",
+  },
+  {
+    id: 8,
+    name: "Best overall",
+    image: "",
+  },
+];
